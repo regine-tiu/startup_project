c4b0267d3a22b3d7ee731239e94922c78c0da55a
regine-tiu <regine.d.tiu@gmail.com>
2021-05-12 13:54:43 +0800

    part 2 solved

--- lib/startup.rb
+++ lib/startup.rb
@@ -1,5 +1,47 @@
 require "employee"
 
 class Startup
+  attr_reader :name, :funding, :salaries, :employees
 
+  def initialize(name, funding, salaries)
+    @name = name
+    @funding = funding
+    @salaries = salaries
+    @employees = []
+  end  
+
+  def valid_title?(title)
+    @salaries.key?(title)
+  end  
+
+  def >(other_startup)
+    self.funding > other_startup.funding
+  end  
+
+  def hire(name, title)
+    if self.valid_title?(title)
+      @employees << Employee.new(name, title)
+    else
+      raise "title is invalid"  
+    end  
+  end  
+
+  def size
+    @employees.length
+  end  
+
+  def pay_employee(employee)
+    employee_salary = @salaries[employee.title]
+
+    if self.funding >= employee_salary
+      employee.pay(employee_salary)
+      @funding -= employee_salary
+    else
+      raise "the startup does not have enough funding"
+    end  
+  end  
+
+  def payday
+    @employees.each {|employee| self.pay_employee(employee)}
+  end  
 end
